bdd8586ab10bf1d33dbbd0cb6c44fcc3656eff11
Lukas Storck <mail@lukasstorck.com>
2023-11-02 11:16:03 +0100

    add link to repo on website, closes #3

--- client-app/src/components/Header.tsx
+++ client-app/src/components/Header.tsx
@@ -7,8 +7,23 @@ interface Props {
 function Header({ children }: Props) {
   return (
     <header>
-      <div className="m-4">
-        <h1 className="text-center">{children}</h1>
+      <div className="m-4 d-flex justify-content-center align-items-center flex-wrap">
+        <h1 className="text-center inline m-0">{children}</h1>
+        <div className="text-center mx-3">
+          <a
+            href="https://github.com/lukasstorck/py-pdf-tools"
+            target="_blank"
+            className="text-muted text-decoration-none text-no-warp"
+          >
+            <span>view source</span>
+            <img
+              src="github-mark.svg"
+              alt="Github Logo"
+              style={{ height: "1rem" }}
+              className="mx-1"
+            />
+          </a>
+        </div>
       </div>
     </header>
   );
